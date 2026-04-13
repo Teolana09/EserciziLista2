4c34e49b87d166b8ed83f3138316b9353fe77c33
Teolana09 <lanari.matteo@franchettisalviani.net>
2026-04-13 08:58:07 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EserciziLista2
+{
+    internal class Program
+    {
+        static List<int> IndiceEsplosivo(List<int> lista)
+        {
+            for (int i = lista.Count - 1; i >= 0; i--)
+            {
+                if (lista[i] == i)
+                {
+                    lista.RemoveAt(i);
+                }
+                
+            }
+            
+            return lista;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Progetto Liste2 13/04/2026");
+            List<int> lista1 = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
+            List<int> lista2 = IndiceEsplosivo (lista1);
+            foreach (int i in lista2)
+            {
+                Console.WriteLine(i);
+            }
+            List<int> listaEs2 = new List<int>() { 1, 2, 3, 4, 5 };
+            Console.WriteLine("inserisci il numero di spostamenti");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
