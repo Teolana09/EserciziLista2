344e2ffbd029a2ba6d38cf3d27660b1c96bfb656
Teolana09 <lanari.matteo@franchettisalviani.net>
2026-04-14 10:51:38 +0200

    mod2

--- Program.cs
+++ Program.cs
@@ -2,6 +2,26 @@
 {
     internal class Program
     {
+        static List<int> PicchiDifrequenza(List<int> liste)
+        {
+
+        }
+        static List<int> Inversione(List<int> liste, int num)
+        {
+           
+            for (int i = 0; i < num; i++)
+            {
+                int j = 0;
+                j = liste[liste.Count - 1];             
+                
+                liste.RemoveAt(liste.Count - 1);
+                liste.Insert(0, j);
+                
+               
+                    
+            }
+            return liste;
+        }
         static List<int> IndiceEsplosivo(List<int> lista)
         {
             for (int i = lista.Count - 1; i >= 0; i--)
@@ -27,6 +47,11 @@
             List<int> listaEs2 = new List<int>() { 1, 2, 3, 4, 5 };
             Console.WriteLine("inserisci il numero di spostamenti");
             int n = Convert.ToInt32(Console.ReadLine());
+            List<int> lista3 = Inversione(listaEs2, n);
+            foreach(int j in lista3)
+            {
+                Console.Write(j);
+            }
 
 
         }
